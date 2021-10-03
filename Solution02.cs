5f3e74743bbe8e63b9bc396a21e6c7fbfc864315
RoKashou <keima.sawamura@gmail.com>
2021-10-03 16:21:22 +0900

    Create Solution02.cs

---
+++ Solution02.cs
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        int[] resultValue = new int[100];
+
+        int count = 0;
+
+        ListNode result = null;
+
+        while (true)
+        {
+            if (l1 != null)
+            {
+                resultValue[count] += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) { 
+                resultValue[count] += l2.val;
+                l2 = l2.next;
+            }
+
+            if (resultValue[count] > 9)
+            {
+                resultValue[count + 1] = resultValue[count] / 10;
+                resultValue[count] = resultValue[count] % 10;
+            }
+
+            count++;
+
+            if (l1 == null && l2 == null)
+            {
+                if(resultValue[count] > 0) count++;
+                break;
+            }
+        }
+
+        for(int index = count-1; index >= 0; index--)
+        {
+            result = new ListNode(resultValue[index], result);
+        }
+
+        return result;
+    }
+}
