9830517b6f077a0e98b5d8fc263ac71e1dd1cc97
RoKashou <keima.sawamura@gmail.com>
2021-10-05 21:09:09 +0900

    Create Solution06.cs

---
+++ Solution06.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if(numRows == 1) return s;
+
+        string ret=string.Empty;
+        int n = s.Length;
+        int cycleLen = 2 * numRows - 2;
+
+        for(int i = 0; i<numRows; i++){
+            for(int j=0;j + i < n; j += cycleLen){
+                ret += s[j+i];
+                if(i!=0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret += s[j + cycleLen - i];
+            }
+        }
+
+        return ret;
+
+    }
+}
