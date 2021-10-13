de334c739cb613040ee486582e6e8fe572edb92c
RoKashou <keima.sawamura@gmail.com>
2021-10-13 22:00:03 +0900

    Create Solution1008.cs

---
+++ Solution1008.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode BstFromPreorder(int[] preorder) {
+        int length = preorder.Length;
+        TreeNode root = new TreeNode(preorder[0]);
+        TreeNode tn = root;
+        int idx=1;
+        while(idx<length){
+            while(preorder[idx] < tn.val && tn.left != null){
+                tn = tn.left;
+            }
+            if(preorder[idx] < tn.val && tn.left==null){
+                tn.left = new TreeNode(preorder[idx]);
+            }
+            while(preorder[idx] > tn.val && tn.right != null){
+                tn = tn.right;
+            }
+            if(preorder[idx] > tn.val && tn.right == null){
+                tn.right = new TreeNode(preorder[idx]);
+            }
+            if(tn.val == preorder[idx]){
+                idx++;
+                tn = root;
+            }
+        }
+        return root;
+        
+    }
+}
