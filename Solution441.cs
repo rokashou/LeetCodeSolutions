f21974f77c7ef57086a15520eac0ae5a9a0ba70f
RoKashou <keima.sawamura@gmail.com>
2021-11-05 17:33:46 +0900

    Create Solution441.cs

---
+++ Solution441.cs
@@ -0,0 +1,22 @@
+public class Solution {
+
+    /*
+        Runtime: 36 ms, faster than 90.28% of C# online submissions for Arranging Coins.
+        Memory Usage: 27 MB, less than 11.11% of C# online submissions for Arranging Coins.
+    */
+    public int ArrangeCoins(int n)
+    {
+        int level = 0;
+
+        int cnt = 1;
+
+        while(n >= cnt)
+        {
+            n -= cnt;
+            cnt+=1;
+            level+=1;
+        }
+
+        return level;
+    }
+}
