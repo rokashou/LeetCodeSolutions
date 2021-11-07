115a34b9c2217ae59dddd85229a6012220b210b1
RoKashou <keima.sawamura@gmail.com>
2021-11-07 20:13:59 +0900

    Update and rename Solution07.cs to Solution0007.cs

---
+++ Solution0007.cs
@@ -0,0 +1,28 @@
+/*
+Runtime: 67 ms, 16.71%
+Memory Usage: 15.7 MB, 49.75%
+*/
+
+public class Solution {
+    public int Reverse(int x) {
+        bool negative = x<0;
+
+        string s = x.ToString();
+        string s2 = string.Empty;
+        for(int i=s.Length-1; i>=0; i--){
+            if(negative && i==0) break;
+            s2 += s[i];
+        }
+
+        if(negative) s2= "-" + s2;
+
+        if(int.TryParse(s2,out int result))
+        {
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
