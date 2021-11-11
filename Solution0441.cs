9ee66279148fd726af0b2eb9f4940f4b059e32e3
RoKashou <keima.sawamura@gmail.com>
2021-11-11 23:13:27 +0900

    Update and rename Solution441.cs to Solution0441.cs

--- Solution0441.cs
+++ Solution0441.cs
@@ -1,6 +1,7 @@
 public class Solution {
 
     /*
+        Uploaded: 11/05/2021 17:31
         Runtime: 36 ms, faster than 90.28% of C# online submissions for Arranging Coins.
         Memory Usage: 27 MB, less than 11.11% of C# online submissions for Arranging Coins.
     */
