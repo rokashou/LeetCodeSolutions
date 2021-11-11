ba606b64acaa4d3c701759b365de596b7f144840
RoKashou <keima.sawamura@gmail.com>
2021-11-11 23:16:30 +0900

    Update and rename Solution422.cs to Solution0442.cs

--- Solution0442.cs
+++ Solution0442.cs
@@ -1,3 +1,9 @@
+/*
+Uploaded: 10/06/2021 19:58
+Runtime: 184 ms, 77.85% better
+Memory Usage: 48.4 MB, 55.26% better
+*/
+
 public class Solution {
     public IList<int> FindDuplicates(int[] nums) {
         int length = nums.Length;
