0dafe1ace9b019f2489d30bed8c407f990c7380f
RoKashou <keima.sawamura@gmail.com>
2022-01-09 00:46:42 +0900

    Create Solution1463.Approach1.cs

---
+++ Solution1463.Approach1.cs
@@ -0,0 +1,55 @@
+/*
+Uploaded: 01/09/2022 00:44
+Runtime: 174 ms, faster than 69.23% of C# online submissions for Cherry Pickup II.
+Memory Usage: 39.1 MB, less than 84.62% of C# online submissions for Cherry Pickup II.
+
+Approach 1 : DP(dynamic programming) - Top Down
+
+*/
+
+public class Solution {
+    public int CherryPickup(int[][] grid) {
+        int m = grid.Length;
+        int n = grid[0].Length;
+        int[,,] dpCache = new int[m,n,n];
+        // initial all elements to -1 to mark unseen
+        for (int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    dpCache[i,j,k]=-1;
+                }
+            }
+        }
+        return dp(0,0,n-1,grid,dpCache);
+    }
+
+    private int dp(int row, int col1, int col2, int[][] grid, int[,,] dpCache){
+        if(col1<0 || col1 >=grid[0].Length || col2 < 0 || col2 >= grid[0].Length){
+            return 0;
+        }
+        // check cache
+        if(dpCache[row,col1,col2]!=-1){
+            return dpCache[row,col1,col2];
+        }
+        // current cell
+        int result = 0;
+        result += grid[row][col1];
+        if(col1 != col2){
+            result += grid[row][col2];
+        }
+        // transition
+        if(row != grid.Length - 1){
+            int max=0;
+            for(int newCol1 = col1 - 1; newCol1<=col1+1;newCol1++){
+                for(int newCol2 = col2 - 1; newCol2 <= col2 + 1;newCol2++){
+                    max = Math.Max(max,dp(row+1,newCol1,newCol2,grid,dpCache));
+
+                }
+            }
+            result += max;
+        }
+
+        dpCache[row,col1,col2]=result;
+        return result;
+    }
+}
