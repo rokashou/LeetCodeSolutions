7230e23b781bc76081cbf146e0d4d169021fa351
RoKashou <keima.sawamura@gmail.com>
2022-01-25 20:57:25 +0900

    Create Solution0709.cs

---
+++ Solution0709.cs
@@ -0,0 +1,11 @@
+/*
+Uploaded: 01/25/2022 20:56
+Runtime: 129 ms, faster than 32.26% of C# online submissions for To Lower Case.
+Memory Usage: 36.6 MB, less than 60.65% of C# online submissions for To Lower Case.
+*/
+
+public class Solution {
+    public string ToLowerCase(string s) {
+        return s.ToLower();
+    }
+}
