d30d0535adcb32bc5ccb91bcd1c64e5e20db17db
RoKashou <keima.sawamura@gmail.com>
2022-04-25 23:35:14 +0900

    Update Solution0284.cs

--- Solution0284.cs
+++ Solution0284.cs
@@ -37,3 +37,40 @@ class PeekingIterator {
         return table.Count > 0;
     }
 }
+
+
+
+// C# IEnumerator interface reference:
+// https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=netframework-4.8
+
+class PeekingIterator_best{
+    private IEnumerator<int> _iter;
+    private bool _hasNext;
+
+    // iterators refers to the first element of the array.
+    public PeekingIterator(IEnumerator<int> iterator)
+    {
+        _iter = iterator;
+        _hasNext = true;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public int Peek()
+    {
+        return _iter.Current;
+    }
+
+    // Returns the next element in the iteration and advances the iterator.
+    public int Next()
+    {
+        var v = _iter.Current;
+        _hasNext = _iter.MoveNext();
+        return v;
+    }
+
+    // Returns false if the iterator is refering to the end of the array of true otherwise.
+    public bool HasNext()
+    {
+        return _hasNext;
+    }
+}
