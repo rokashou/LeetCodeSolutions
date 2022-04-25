1f3fb00cc48b2239c58bcbd5ceddd84927ee1207
RoKashou <keima.sawamura@gmail.com>
2022-04-25 23:20:02 +0900

    Create Solution0284.cs

---
+++ Solution0284.cs
@@ -0,0 +1,39 @@
+/*
+Runtime: 154 ms, faster than 57.14% of C# online submissions for Peeking Iterator.
+Memory Usage: 40.1 MB, less than 92.86% of C# online submissions for Peeking Iterator.
+Uploaded: 04/25/2022 23:18
+*/
+
+
+
+// C# IEnumerator interface reference:
+// https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=netframework-4.8
+
+class PeekingIterator {
+    Queue<int> table = new Queue<int>();
+
+    // iterators refers to the first element of the array.
+    public PeekingIterator(IEnumerator<int> iterator)
+    {
+        table.Enqueue(iterator.Current);
+        while (iterator.MoveNext()) table.Enqueue(iterator.Current);
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public int Peek()
+    {
+        return table.Peek();
+    }
+
+    // Returns the next element in the iteration and advances the iterator.
+    public int Next()
+    {
+        return table.Dequeue();
+    }
+
+    // Returns false if the iterator is refering to the end of the array of true otherwise.
+    public bool HasNext()
+    {
+        return table.Count > 0;
+    }
+}
