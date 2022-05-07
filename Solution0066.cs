bface61dc311b31404d17e9d128a9e1e78450300
RoKashou <keima.sawamura@gmail.com>
2022-05-08 01:55:42 +0900

    Add faster approach

--- Solution0066.cs
+++ Solution0066.cs
@@ -22,3 +22,45 @@ public class Solution {
     }
 }
 
+
+
+/*
+Faster if dont use List.ToArray()
+
+Runtime: 146 ms, faster than 77.14% of C# online submissions for Plus One.
+Memory Usage: 40.7 MB, less than 93.79% of C# online submissions for Plus One.
+Uploaded: 05/08/2022 01:53
+*/
+public class Solution {
+    public int[] PlusOne(int[] digits)
+    {
+        int carry = 1;
+        for(int i = digits.Length - 1; i >= 0; i--)
+        {
+            if(digits[i]+carry > 9)
+            {
+                digits[i] = 0;
+                carry = 1;
+            }
+            else
+            {
+                digits[i] += 1;
+                carry = 0;
+                break; // can break if there is no carry up.
+            }
+        }
+        if (carry == 1)
+        {
+            int[] result = new int[digits.Length + 1];
+            result[0] = 1;
+            for(int i = 0; i < digits.Length; i++)
+            {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+
+        return digits;
+    }
+}
+
