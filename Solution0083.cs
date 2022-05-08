f9c44c0b18e57717da21ad03e1aef133f83e1254
RoKashou <keima.sawamura@gmail.com>
2022-05-08 23:26:34 +0900

    Add Best Solution

--- Solution0083.cs
+++ Solution0083.cs
@@ -3,18 +3,6 @@ Runtime: 142 ms, faster than 18.85% of C# online submissions for Remove Duplicat
 Memory Usage: 38.2 MB, less than 55.78% of C# online submissions for Remove Duplicates from Sorted List.
 Uploaded: 05/08/2022 23:11
 */
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
  
 public class Solution {
     public ListNode DeleteDuplicates(ListNode head) {
@@ -36,3 +24,43 @@ public class Solution {
         return head;
     }
 }
+
+
+/*
+Runtime: 119 ms, faster than 44.30% of C# online submissions for Remove Duplicates from Sorted List.
+Memory Usage: 38.5 MB, less than 36.62% of C# online submissions for Remove Duplicates from Sorted List.
+Uploaded: 05/08/2022 23:23
+*/
+ 
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode node = head;
+
+        while(node != null)
+        {
+            ListNode nextNode = node.next;
+            while(nextNode != null && nextNode.val == node.val)
+            {
+                nextNode = nextNode.next;
+            }
+            node.next = nextNode;
+            node = nextNode;                
+        }
+
+        return head;
+    }
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
