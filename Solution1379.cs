81349f27a64577897c55dab5a1c7c09c07d8b7dc
RoKashou <keima.sawamura@gmail.com>
2022-05-17 21:22:53 +0900

    Create Solution1379.cs

---
+++ Solution1379.cs
@@ -0,0 +1,35 @@
+/*
+Runtime: 461 ms, faster than 34.43% of C# online submissions for Find a Corresponding Node of a Binary Tree in a Clone of That Tree.
+Memory Usage: 48 MB, less than 96.72% of C# online submissions for Find a Corresponding Node of a Binary Tree in a Clone of That Tree.
+Uploaded: 05/17/2022 21:21
+*/
+
+public class Solution {
+    public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target) {
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        stack1.Push(original);
+        stack2.Push(cloned);
+
+        while (stack1.Count > 0)
+        {
+            var node1 = stack1.Pop();
+            var node2 = stack2.Pop();
+            if (object.ReferenceEquals(node1,target))
+            {
+                return node2;
+            }
+            if (node1.left != null)
+            {
+                stack1.Push(node1.left);
+                stack2.Push(node2.left);
+            }
+            if (node1.right != null)
+            {
+                stack1.Push(node1.right);
+                stack2.Push(node2.right);
+            }
+        }
+        return null;
+    }
+}
