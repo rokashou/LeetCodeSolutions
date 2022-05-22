4396a849b05bfd9b8ce05e03271144e773bcf446
RoKashou <keima.sawamura@gmail.com>
2022-05-22 16:37:31 +0900

    Create Solution2235.cs

---
+++ Solution2235.cs
@@ -0,0 +1,11 @@
+/*
+Runtime: 26 ms, faster than 74.17% of C# online submissions for Add Two Integers.
+Memory Usage: 26.5 MB, less than 28.24% of C# online submissions for Add Two Integers.
+Uploaded: 05/22/2022 16:36
+*/
+
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
