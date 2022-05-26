2f932333057e862f66a20d3f1218b0055b597c43
RoKashou <keima.sawamura@gmail.com>
2022-05-27 00:07:04 +0900

    Update Solution1873.sql

--- Solution1873.sql
+++ Solution1873.sql
@@ -1,9 +1,5 @@
 SELECT 
     employee_id,
-    CASE
-        WHEN employee_id % 2 = 0 THEN 0
-        WHEN name LIKE 'M%' THEN 0
-        ELSE salary
-    END as bonus
+    IF(employee_id % 2 = 0 or name LIKE 'M%',0,salary) as bonus
 FROM Employees
 ORDER BY employee_id
