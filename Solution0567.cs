b4f0a56c9a4211607bd3b44e77dee7af3cebd730
RoKashou <keima.sawamura@gmail.com>
2023-02-06 01:58:08 +0900

    Update Solution0567.cs

--- Solution0567.cs
+++ Solution0567.cs
@@ -1,7 +1,47 @@
 /*
+Runtime 2406 ms, Beats 5.15%
+Memory 51.3 MB, Beats 7.17%
+Feb 06, 2023 00:17
+*/
+
+public class Solution {
+    public bool CheckInclusion(string s1, string s2)
+    {
+        if(s1.Length > s2.Length) return false;
+
+        for (int i = 0; i <= s2.Length - s1.Length; i++){
+            if (CheckPermutation(s1, s2.Substring(i, s1.Length)))
+                return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckPermutation(string s1, string s2)
+    {
+        char[] s1map = s1.ToCharArray();
+        char[] s2map = s2.ToCharArray();
+        Array.Sort(s1map);
+        Array.Sort(s2map);
+        for(int i = 0; i < s1.Length; i++)
+        {
+            if (s1map[i] != s2map[i]) return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+/*
 Runtime: 117 ms, faster than 62.64% of C# online submissions for Permutation in String.
 Memory Usage: 37.7 MB, less than 63.25% of C# online submissions for Permutation in String.
 Uploaded: 02/11/2022 23:26
+
+Not pass: Feb 06, 2023
 */
 
 
