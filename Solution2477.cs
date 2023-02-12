7b98edf0569d672e41b16361a7c220576aefd966
RoKashou <keima.sawamura@gmail.com>
2023-02-12 12:56:32 +0900

    Update Solution2477.cs

--- Solution2477.cs
+++ Solution2477.cs
@@ -1,4 +1,58 @@
 /*
+DFS Best Solution
+Runtime 531 ms, Beats 100%
+Memory 77.4 MB, Beats 63.64%
+Feb 12, 2023 11:54
+*/
+
+public class Solution {
+    private int _seatSize;
+    private long ans = 0;
+
+    public long MinimumFuelCost(int[][] roads, int seats)
+    {
+        var graph = new List<IList<int>>();
+        _seatSize = seats;
+
+        // initial the graph
+        for (int i = 0; i < roads.Length + 1; i++)
+        {
+            graph.Add(new List<int>());
+        }
+
+        foreach(var road in roads)
+        {
+            graph[road[0]].Add(road[1]);
+            graph[road[1]].Add(road[0]);
+        }
+
+        CountPeople(0, 0, graph);
+
+        return ans;
+
+    }
+
+    private long CountPeople(int node, int prev, List<IList<int>> graph)
+    {
+        long people = 1; // the passenger from current node
+
+        foreach(int x in graph[node])
+        {
+            if (x == prev) continue;
+            people += CountPeople(x, node, graph);
+        }
+        if (node != 0)
+        {
+            ans += (people + _seatSize - 1) / _seatSize;
+        }
+
+        return people;
+    }
+}
+
+
+
+/*
 Runtime 823 ms, Beats 36.36%
 Memory 118.9 MB, Beats 9.9%
 Feb 12, 2023 11:41
