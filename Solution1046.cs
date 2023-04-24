7851b845000749caabdf60eed5a66419d7e743de
RoKashou <keima.sawamura@gmail.com>
2023-04-24 21:43:34 +0900

    Update Solution1046.cs

--- Solution1046.cs
+++ Solution1046.cs
@@ -1,4 +1,27 @@
 /*
+Apr 24, 2023 20:40
+Runtime 97 ms Beats 10.23%
+Memory 37.7 MB Beats 90.6%
+*/
+
+public class Solution {
+    public int LastStoneWeight(int[] stones) {
+        var pq = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y - x));
+        foreach (var stone in stones)
+            pq.Enqueue(stone, stone);
+
+        while(pq.Count > 1){
+            int a = pq.Dequeue();
+            int b = pq.Dequeue();
+            a -= b;
+            if(a>0) pq.Enqueue(a,a);
+        }
+        return pq.Count == 0 ? 0 : pq.Dequeue();
+    }
+}
+
+
+/*
 Runtime: 140 ms, faster than 22.17% of C# online submissions for Last Stone Weight.
 Memory Usage: 37.1 MB, less than 33.00% of C# online submissions for Last Stone Weight.
 Uploaded: 04/07/2022 23:37
