8e3c84304d955def16f9dbeb90a91f9b7d35ddb9
RoKashou <keima.sawamura@gmail.com>
2023-04-27 22:38:36 +0900

    Create Solution0319.cs

---
+++ Solution0319.cs
@@ -0,0 +1,12 @@
+/*
+Apr 27, 2023 21:37
+Runtime 24 ms Beats 69.57%
+Memory 26.6 MB Beats 60.87%
+*/
+
+
+public class Solution {
+    public int BulbSwitch(int n) {
+        return (int)Math.Sqrt(n);   
+    }
+}
