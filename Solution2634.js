2ef9716d5dce2d7fbcf2fe7daf190e6426a183c3
RoKashou <keima.sawamura@gmail.com>
2023-05-02 22:39:20 +0900

    Add files via upload

---
+++ Solution2634.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+    newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        element = arr[i];
+        if (fn(element, i)) {
+            newArr.push(element);
+        }
+    }
+    return newArr;
+};
