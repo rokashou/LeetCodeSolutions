f2a4bba76b65be943ce9e33f85f150c86e32f273
RoKashou <keima.sawamura@gmail.com>
2023-05-28 21:40:24 +0900

    Update Solution0399.cs

--- Solution0399.cs
+++ Solution0399.cs
@@ -1,4 +1,63 @@
 /*
+May 28, 2023 20:38
+Runtime 150 ms Beats 81.85%
+Memory 43 MB Beats 88.58%
+*/
+
+public class Solution {
+    public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries) {
+        double[] ans = new double[queries.Count];
+        var graph = new Dictionary<string, Dictionary<string, double>>();
+
+        for (int i = 0; i < equations.Count; ++i) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            Dictionary<string, double> first;
+            if (!graph.TryGetValue(a, out first)) {
+                first = new Dictionary<string, double>();
+                graph.Add(a, first);
+            }
+            first[b] = values[i];
+            Dictionary<string, double> second;
+            if (!graph.TryGetValue(b, out second)) {
+                second = new Dictionary<string, double>();
+                graph.Add(b, second);
+            }
+            second[a] = 1 / values[i];
+        }
+
+        for (int i = 0; i < queries.Count; i++) {
+            IList<string> query = queries[i];
+            string a = query[0];
+            string c = query[1];
+            if (!graph.ContainsKey(a) || !graph.ContainsKey(c))
+                ans[i] = -1;
+            else
+                ans[i] = Divide(graph, a, c, new HashSet<string>());
+        }
+        return ans;
+    }
+    double Divide(Dictionary<string, Dictionary<string, double>> graph, string a, string c, HashSet<string> seen) {
+        if (a == c)
+            return 1.0;
+        seen.Add(a);
+        Dictionary<string, double> nodes;
+        if (!graph.TryGetValue(a, out nodes))
+            return -1;
+        foreach (var item in graph[a]) {
+            if (seen.Contains(item.Key))
+                continue;
+            double res = Divide(graph, item.Key, c, seen);
+            if (res > 0)
+                return item.Value * res;
+        }
+        return -1;
+    }
+}
+
+
+
+/*
 Runtime: 160 ms, faster than 78.92% of C# online submissions for Evaluate Division.
 Memory Usage: 43.2 MB, less than 11.21% of C# online submissions for Evaluate Division.
 Uploaded: 05/01/2022 01:16
