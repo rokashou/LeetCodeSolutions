94bad85a0895065bc7f0f95ce87cd048f281c698
RoKashou <keima.sawamura@gmail.com>
2024-12-02 17:27:56 +0900

    Create solution1455.rs

---
+++ Rust/solution1455.rs
@@ -0,0 +1,29 @@
+/*
+Dec 02, 2024 17:26
+Runtime 0ms, Beats 100.00%
+Memory 2.23MB, Beats 29.41%
+*/
+
+impl Solution {
+    pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
+        let mut word_idx = 0;
+        let mut i = 0;
+
+        loop {
+            word_idx += 1;
+
+            // Check if the substring starting at `i` matches `search_word`
+            if sentence[i..].starts_with(&search_word) {
+                return word_idx;
+            }
+
+            // Find the next space in the sentence
+            match sentence[i..].find(' ') {
+                Some(pos) => i += pos + 1, // Move to the next word
+                None => break,            // No more words in the sentence
+            }
+        }
+
+        -1
+    }
+}
